63019d57811a61e6cb117825fee2fdc4656ed640
Mario Hernandez <mhmario963@gmail.com>
2023-01-09 15:19:16 -0700

    Scaffold repository.

---
+++ array_problems/PlusOneSolution.py
@@ -0,0 +1,6 @@
+import sys
+from typing import List
+
+class PlusOneSolution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        return [];
---
+++ main.py
@@ -0,0 +1,10 @@
+from array_problems.PlusOneSolution import PlusOneSolution
+
+def main() -> int:
+    test = PlusOneSolution()
+    test.plusOne([1])
+
+    return 0
+
+if __name__ == "__main__":
+    main()
