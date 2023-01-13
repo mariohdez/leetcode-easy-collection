12191428abfc72ad866b74317fd87a2950917984
Mario Hernandez <mhmario963@gmail.com>
2023-01-13 11:40:46 -0700

    fix reverse integer in python

--- string_problems/reverse_integer.py
+++ string_problems/reverse_integer.py
@@ -1,20 +1,20 @@
+import math
 import sys
 
 class ReverseIntegerSolution:
     def reverse(self, x: int) -> int:
         reversedX = 0
+        MIN = -2147483648
+        MAX = 2147483647
 
-        # Look at neetcode.io's solution because python has some weirdness with it's number data type implementation. #
+        while x:
+            remainder = int(math.fmod(x, 10))
+            x = int(x / 10)
 
-        # while x > 0:
-        #     remainder = x % 10
-
-        #     if reversedX > sys.maxsize//10 or (reversedX == sys.maxsize//10 and remainder > 7):
-        #         return 0
-        #     if reversedX < (-sys.maxsize - 1)//10 or reversedX == (-sys.maxsize - 1)//10 and remainder < -8:
-        #         return 0
-
-        #     reversedX = (reversedX * 10) + remainder
-        #     x = x // 10
+            if reversedX > MAX // 10 or (reversedX == MAX // 10 and remainder > 7):
+                return 0
+            if reversedX < MIN // 10 or (reversedX == MIN // 10 and  reversedX < -8):
+                return 0
+            reversedX = (reversedX * 10) + remainder
 
         return reversedX
